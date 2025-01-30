c96ae4ce76d0bd93daf75844418154f3586ae8cc
Imanm02 <imanm1381@gmail.com>
2025-01-30 05:34:29 +0330

    Update SPDK fio script
    Co-authored-by: Iman Mohammadi <imanm1381@gmail.com>
    Co-authored-by: Kiarash Joolaei <kjoolaiy@gmail.com>

---
+++ Experiments/SPDK/script3.sh
@@ -0,0 +1,30 @@
+SPDK_FIO=~/spdk-master/build/fio/spdk_nvme
+export LD_PRELOAD=$SPDK_FIO
+
+types=("randrw" "randwrite" "randread")
+sizes=(4 1)
+numjobs=(1 4)
+
+for type in "${types[@]}"; do
+    for i in {0..1}; do
+        output_file="${type}_$((i+1)).txt"
+        
+        echo "type: $type, size: ${sizes[i]}, numjobs= ${numjobs[i]}"
+        echo "type: $type, size: ${sizes[i]}, numjobs= ${numjobs[i]}" > "$output_file"
+
+        echo "genesis1:3" | sudo -S fio \
+            --filename=/dev/nvme0n1 \
+            --readwrite=$type \
+            --name=randwrite \
+            --blocksize=4kb \
+            --direct=1 \
+            --ioengine=spdk \
+            --size=${sizes[i]}G \
+            --numjobs=${numjobs[i]} \
+            --time_based=1 \
+            --runtime=10 \
+            --refill_buffers \
+            --norandommap \
+            --group_reporting >> "$output_file"
+    done
+done
